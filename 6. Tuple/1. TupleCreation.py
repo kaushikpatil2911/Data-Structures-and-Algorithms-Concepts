e7533bb2053a8c7a331a58e7beda5b5ee368057d
Kaushik Patil <kaushspyker@gmail.com>
2024-02-11 22:24:47 -0600

    Tuples codes added

---
+++ 6. Tuple/1. TupleCreation.py
@@ -0,0 +1,13 @@
+newTuple = ('a','b','c','d','e')
+print(newTuple)
+newTuple = ('a',)
+print(newTuple)
+
+newTuple = tuple()
+print(newTuple)
+
+newTuple = tuple('abcde')
+print(newTuple)
+
+# Time Complexity -> O(1)
+# Space Complexity -> O(N)
---
+++ 6. Tuple/2. TupleAccess.py
@@ -0,0 +1,6 @@
+newTuple = ('a','b','c','d','e')
+print(newTuple[1])
+print(newTuple[-2])
+
+print(newTuple[1:3])
+print(newTuple[::2])
