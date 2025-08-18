564e7ea2350ea3047476e1bbe29146e80a99aa34
Kaushik Patil <kaushspyker@gmail.com>
2025-08-17 23:24:06 -0400

    Updated files

--- 2. Array/9. 2DArray Accessing.py
+++ 2. Array/9. 2DArray Accessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-twoDArray = np.array([[11,15,10,6],[10,14,11,5],[12,17,12,8],[15,18,14,9]])       
+twoDArray = np.array([[11,15,10,6],[10,14,11,5],[12,17,12,8],[15,18,14,9]])    
+print(twoDArray)   
 
 def accessElements(array, rowIndex, columnIndex):
     if rowIndex >= len(array) and columnIndex >= len(array[0]):
